74115b4a9963a4d64f3b1c4201c93131e2b431b4
HNoobcoderha <markisrael099@icloud.com>
2024-08-24 22:00:01 +0800

    Update prac.c

--- prac.c
+++ prac.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
 
-
+
+int main(){
+  
+  int age;
+  age: 45
+  
+  
+  printf("input age:");
+  scanf("%d",&age);
+  
+  
+  if(age <= 45)
+  { 
+    printf("You are eligable!"); 
+  }
+  else if
+  {
+    printf("You are not eligavle!!");
+  }
+  
+  
+  
+  return 0;
+}
